bc7c23b839f164e831c95b1dc30def903c94206e
leanonybr-7579 <ulissescarvalho.d@gmail.com>
2020-04-10 16:39:36 -0300

    frontend

---
+++ frontend/src/services/fetch.js
@@ -0,0 +1,39 @@
+import { api } from './api'
+import { addPlayerToChat, logOut, error409, error404 } from '../store/actions'
+
+export function PUT(route, data) {
+    return async dispatch => {
+        try {
+            const json = await api.put(route, data)
+            const { name } = json.data
+            console.log(name)
+            if (json.data.status === 200) {
+                dispatch(addPlayerToChat(name))
+            }
+            else if (json.data.status === 409) {
+                dispatch(error409(name))
+            }
+        }
+        catch (err) {
+            return console.error(err)
+        }
+    }
+}
+
+export function DELETE(route, param) {
+    return async dispatch => {
+        try {
+            const json = await api.delete(`${route}/${param}`)
+            const { name } = json.data
+            if (json.status === 404) {
+                dispatch(error404(name))
+            }
+            else if (json.status === 200) {
+                dispatch(logOut(name))
+            }
+        }
+        catch (err) {
+            return console.error(err)
+        }
+    }
+}
